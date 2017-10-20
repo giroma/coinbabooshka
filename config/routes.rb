0854e30e95f9ef0eb90705692152dd4f20b1c0e2
Argir Argirov <dj.giro@gmail.com>
2017-10-20 15:38:22 -0400

    routes and models and controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'coins#index'
+  resources :coins
+  resource :users, only: [:create, :new ]
+  resource :sessions, only: [:create, :new, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
